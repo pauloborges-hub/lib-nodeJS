aeee1c570fb47b1f42b9de6475162988ddd2a44c
Paulo Borges <victorborges296@hotmail.com>
2024-02-06 08:42:02 -0300

    tratamento de erros

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import chalk from "chalk";
+import fs from "fs";
 
-console.log(chalk.blue('olá mundo'));
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+
+function trataErro(erro) {
+    // error é um objeto do js, tem as propriedades errno, code, syscal e path
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+pegaArquivo('./arquivos/.md');
