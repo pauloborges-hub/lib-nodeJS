cac80b6364e08f7ec7aba3efa925c04ce375912d
Paulo Borges <victorborges296@hotmail.com>
2024-02-07 08:38:42 -0300

    capturando grupos

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import chalk from "chalk";
 import fs from "fs";
 
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/;
+    const caputuras = regex.exec(texto);
+    console.log(caputuras);
+}
 
 function trataErro(erro) {
     // error é um objeto do js, tem as propriedades errno, code, syscal e path
@@ -19,23 +26,9 @@ async function pegaArquivo(caminhoDoArquivo) {
     }
 }
 
-// função assincrona com then()
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)               // lê o arquivo
-//         .then((texto) => console.log(chalk.green(texto)))   // assim que terminar de ler, exibe no terminal
-//         .catch(trataErro);                                  // assim que terminar de ler, se erro = true, chama a função trataErro com erro como parâmetro
-// }
+extraiLinks(textoTeste);
 
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if(erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     })
-// }
+// pegaArquivo('./arquivos/.md');
 
-pegaArquivo('./arquivos/.md');
+// const captura1 = /\[[^[\]]*?\]/gm;
+// const captura2 = \(https?:\/\/[^\s?#.].[^\s]*\)/gm;
