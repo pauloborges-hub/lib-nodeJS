c0dada561219e5b6cd1584f6f00b7e7e5c50bb10
Paulo Borges <victorborges296@hotmail.com>
2024-02-08 15:28:51 -0300

    validando links

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -2,8 +2,20 @@ function extraiLinks(arrLinks) {
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
-function listaValidada(listaDeLinks) {
-    return extraiLinks(listaDeLinks);
+async function checaStatus(listaURLs) {
+    const arrStatus = await Promise.all(
+        listaURLs.map(async (url) => {
+            const response = await fetch(url);
+            return response.status;
+        })
+    );
+    return arrStatus;
+}
+
+async function listaValidada(listaDeLinks) {
+    const links = extraiLinks(listaDeLinks);
+    const status = await checaStatus(links);
+    return status;
 }
 
 export default listaValidada;
